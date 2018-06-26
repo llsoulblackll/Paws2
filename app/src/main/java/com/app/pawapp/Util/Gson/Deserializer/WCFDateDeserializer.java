e4f40b1133d3e7a44a4286817b2208e0d51d8843
avaldezn <adrian@autofintech.la>
2018-06-25 19:02:40 -0500

    Implementing auth by tokens

--- app/src/main/java/com/app/pawapp/Util/Gson/Deserializer/WCFDateDeserializer.java
+++ app/src/main/java/com/app/pawapp/Util/Gson/Deserializer/WCFDateDeserializer.java
@@ -6,6 +6,8 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonParseException;
 
 import java.lang.reflect.Type;
+import java.text.DateFormat;
+import java.text.ParseException;
 import java.util.Date;
 
 public class WCFDateDeserializer implements JsonDeserializer<Date> {
@@ -13,11 +15,20 @@ public class WCFDateDeserializer implements JsonDeserializer<Date> {
     @Override
     public Date deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
         String d = json.getAsJsonPrimitive().getAsString();
-        if(d.indexOf('-') > -1)
-            return new Date(Long.parseLong(d.substring(d.indexOf('(') + 1, d.indexOf('-'))));
-        else if(d.indexOf('+') > -1)
-            return new Date(Long.parseLong(d.substring(d.indexOf('(') + 1, d.indexOf('+'))));
-        else
-            return new Date(Long.parseLong(d.substring(d.indexOf('(') + 1, d.indexOf(')'))));
+        System.out.println(d);
+        if(d.indexOf('(') > -1 && d.indexOf(')') > -1) {
+            if (d.indexOf('-') > -1)
+                return new Date(Long.parseLong(d.substring(d.indexOf('(') + 1, d.indexOf('-'))));
+            else if (d.indexOf('+') > -1)
+                return new Date(Long.parseLong(d.substring(d.indexOf('(') + 1, d.indexOf('+'))));
+            else
+                return new Date(Long.parseLong(d.substring(d.indexOf('(') + 1, d.indexOf(')'))));
+        }
+
+        try {
+            return DateFormat.getDateInstance(DateFormat.LONG).parse(d);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
